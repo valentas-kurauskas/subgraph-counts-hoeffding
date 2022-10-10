b927341a1434e0ed246bede4a7245949751483b9
Valentas <valentas@gmail.com>
2022-10-10 12:16:52 +0300

    add combined waves

--- bics_save_waves.R
+++ bics_save_waves.R
@@ -7,3 +7,7 @@ b=a[, .(n=.N, weight=sum(weight_pooled)), by=.(wave, num_cc)]
 dummy<-lapply(0:3, function(x) fwrite(b[wave==x, .(degree=num_cc, freq=n)][order(degree),], paste0("COVID19-Wave",x,".csv"), row.names=FALSE))
 dummy<-lapply(0:3, function(x) fwrite(b[wave==x, .(degree=num_cc, freq=weight/sum(weight))][order(degree),], paste0("COVID19-Wave",x,"_adjusted.csv"), row.names=FALSE))
 
+fwrite(b[wave %in% 1:3, .(freq=sum(n)), by=.(degree=num_cc)], "COVID19-Waves1,2,3combined.csv",row.names=FALSE)
+b[, `:=`(nweight=weight/sum(weight), num_cc_wave=sum(num_cc)), by=.(wave)]
+fwrite(b[wave %in% 1:3, .(freq=sum(nweight*num_cc_wave)/sum(num_cc_wave)), by=.(degree=num_cc)][order(degree)], "COVID19-Waves1,2,3combined_adjusted.csv",row.names=FALSE)
+
